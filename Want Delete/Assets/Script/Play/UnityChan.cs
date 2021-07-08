f12cd554a3acf046f0cfd392507b0391d2e0d1e6
Nyarll <oss314y08@i.softbank.jp>
2021-07-08 16:52:58 +0900

    update : Player Character "UnityChan"

---
+++ Want Delete/Assets/Script/Play/UnityChan.cs
@@ -0,0 +1,114 @@
+﻿//
+// Mecanimのアニメーションデータが、原点で移動しない場合の Rigidbody付きコントローラ
+// サンプル
+// 2014/03/13 N.Kobyasahi
+//
+using UnityEngine;
+using System.Collections;
+
+namespace UnityChan
+{
+    // 必要なコンポーネントの列記
+    [RequireComponent(typeof(Animator))]
+
+    public class UnityChan : MonoBehaviour
+    {
+
+        [SerializeField]
+        public float animSpeed = 1.5f;              // アニメーション再生速度設定
+        [SerializeField]
+        public float lookSmoother = 3.0f;           // a smoothing setting for camera motion
+        [SerializeField]
+        public bool useCurves = true;               // Mecanimでカーブ調整を使うか設定する
+                                                    // このスイッチが入っていないとカーブは使われない
+
+        [SerializeField]
+        public float useCurvesHeight = 0.5f;        // カーブ補正の有効高さ（地面をすり抜けやすい時には大きくする）
+
+        private Animator anim;                          // キャラにアタッチされるアニメーターへの参照
+        private AnimatorStateInfo currentBaseState;         // base layerで使われる、アニメーターの現在の状態の参照
+
+        // アニメーター各ステートへの参照
+        static int idleState = Animator.StringToHash("Base Layer.Idle");
+        static int locoState = Animator.StringToHash("Base Layer.Locomotion");
+        static int jumpState = Animator.StringToHash("Base Layer.Jump");
+        static int restState = Animator.StringToHash("Base Layer.Rest");
+
+        // 初期化
+        void Start()
+        {
+            // Animatorコンポーネントを取得する
+            anim = GetComponent<Animator>();
+        }
+
+
+        // 以下、メイン処理.リジッドボディと絡めるので、FixedUpdate内で処理を行う.
+        void FixedUpdate()
+        {
+            float h = Input.GetAxis("Horizontal");              // 入力デバイスの水平軸をhで定義
+            float v = Input.GetAxis("Vertical");                // 入力デバイスの垂直軸をvで定義
+            anim.SetFloat("Speed", v);                          // Animator側で設定している"Speed"パラメタにvを渡す
+            anim.SetFloat("Direction", h);                      // Animator側で設定している"Direction"パラメタにhを渡す
+            anim.speed = animSpeed;                             // Animatorのモーション再生速度に animSpeedを設定する
+            currentBaseState = anim.GetCurrentAnimatorStateInfo(0); // 参照用のステート変数にBase Layer (0)の現在のステートを設定する
+
+
+
+            if (Input.GetButtonDown("Jump"))
+            {   // スペースキーを入力したら
+
+                //アニメーションのステートがLocomotionの最中のみジャンプできる
+                if (currentBaseState.nameHash == locoState)
+                {
+                    //ステート遷移中でなかったらジャンプできる
+                    if (!anim.IsInTransition(0))
+                    {
+                        anim.SetBool("Jump", true);     // Animatorにジャンプに切り替えるフラグを送る
+                    }
+                }
+            }
+            
+            if (currentBaseState.nameHash == jumpState)
+            {                      
+                // ステートがトランジション中でない場合
+                if (!anim.IsInTransition(0))
+                {
+                    // Jump bool値をリセットする（ループしないようにする）				
+                    anim.SetBool("Jump", false);
+                }
+            }
+            // IDLE中の処理
+            // 現在のベースレイヤーがidleStateの時
+            else if (currentBaseState.nameHash == idleState)
+            {
+                // スペースキーを入力したらRest状態になる
+                if (Input.GetButtonDown("Jump"))
+                {
+                    anim.SetBool("Rest", true);
+                }
+            }
+            // REST中の処理
+            // 現在のベースレイヤーがrestStateの時
+            else if (currentBaseState.nameHash == restState)
+            {
+                //cameraObject.SendMessage("setCameraPositionFrontView");		// カメラを正面に切り替える
+                // ステートが遷移中でない場合、Rest bool値をリセットする（ループしないようにする）
+                if (!anim.IsInTransition(0))
+                {
+                    anim.SetBool("Rest", false);
+                }
+            }
+        }
+
+        void OnGUI()
+        {
+            GUI.Box(new Rect(Screen.width - 260, 10, 250, 150), "Interaction");
+            GUI.Label(new Rect(Screen.width - 245, 30, 250, 30), "Up/Down Arrow : Go Forwald/Go Back");
+            GUI.Label(new Rect(Screen.width - 245, 50, 250, 30), "Left/Right Arrow : Turn Left/Turn Right");
+            GUI.Label(new Rect(Screen.width - 245, 70, 250, 30), "Hit Space key while Running : Jump");
+            GUI.Label(new Rect(Screen.width - 245, 90, 250, 30), "Hit Spase key while Stopping : Rest");
+            GUI.Label(new Rect(Screen.width - 245, 110, 250, 30), "Left Control : Front Camera");
+            GUI.Label(new Rect(Screen.width - 245, 130, 250, 30), "Alt : LookAt Camera");
+        }
+    }
+}
